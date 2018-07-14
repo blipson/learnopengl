485163411e41225e01fc7a4ef2f811df8092a213
Ben Lipson <sonofblip@gmail.com>
2018-07-13 19:54:26 -0500

    first commit

---
+++ cmake-build-debug/CMakeFiles/learopengl.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/learopengl.dir/main.cpp.o"
+  "learopengl.pdb"
+  "learopengl"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/learopengl.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
